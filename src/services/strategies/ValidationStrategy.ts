bed481e10023f01a4165944c4979cf9526016717
andrew <andrew_gallacher@hotmail.com>
2022-10-24 13:42:52 +0100

    Enhance validation strategy to include check on duplicate solutions

--- src/services/strategies/ValidationStrategy.ts
+++ src/services/strategies/ValidationStrategy.ts
@@ -9,9 +9,46 @@ export class ValidationStrategy implements IStrategy {
 
         console.log('ValidationStrategy');
 
+        const rowSolutionCounts: number[][] = [];
+        const columnSolutionCounts: number[][] = [];
+        const squareSolutionCounts: number[][] = [];
+
+        // Set up array of the count of each solution
+        for (let i = 0; i < 9; i++) {
+            rowSolutionCounts.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+            columnSolutionCounts.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+            squareSolutionCounts.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        }
+
         cells.forEach(cell => {
+            // Have we run out of possible solutions?
             if (cell.possibleSolutions.length === 0) {
                 throw new Error(`Row ${cell.rowIndex + 1}, column ${cell.columnIndex + 1} has no possible solutions`);
+            }
+
+            // Do we have a duplicate solution
+            if (cell.possibleSolutions.length === 1) {
+
+                const solutionIndex = cell.possibleSolutions[0] - 1;
+
+                // Check row
+                if (rowSolutionCounts[cell.rowIndex][solutionIndex] > 0) {
+                    throw new Error(`Row ${cell.rowIndex + 1} has duplicate solution for ${solutionIndex + 1}`);
+                }
+
+                // Check column
+                if (columnSolutionCounts[cell.columnIndex][solutionIndex] > 0) {
+                    throw new Error(`Column ${cell.columnIndex + 1} has duplicate solution for ${solutionIndex + 1}`);
+                }
+
+                // Check square
+                if (squareSolutionCounts[cell.squareIndex][solutionIndex] > 0) {
+                    throw new Error(`Square ${cell.squareIndex + 1} has duplicate solution for ${solutionIndex + 1}`);
+                }
+
+                rowSolutionCounts[cell.rowIndex][solutionIndex] = 1;
+                columnSolutionCounts[cell.columnIndex][solutionIndex] = 1;
+                squareSolutionCounts[cell.squareIndex][solutionIndex] = 1;
             }
         });
 
