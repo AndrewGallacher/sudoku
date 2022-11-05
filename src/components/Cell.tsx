6037997f1e5be30d694e40aa0a3f14ae80b92609
andrew <andrew_gallacher@hotmail.com>
2022-11-05 11:22:04 +0000

    enhance cell input

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,40 +1,59 @@
-import React from 'react';
-import { CellModel } from '../models/CellModel';
+import React, { useState } from "react";
+import { CellModel } from "../models/CellModel";
 
 interface CellProps {
-    cell: CellModel,
-    solveCell: (arg0: number, arg1: number, arg2: number) => void
+  cell: CellModel;
+  solveCell: (arg0: number, arg1: number, arg2: number) => void;
 }
 
-const Cell: React.FunctionComponent<CellProps> = ({ cell, solveCell }): JSX.Element => {
+const Cell: React.FunctionComponent<CellProps> = ({
+  cell,
+  solveCell,
+}): JSX.Element => {
+  const [editMode, setEditMode] = useState(false);
 
-    const handleClick = () => {
-        const solution = parseInt(window.prompt('Solution?') ?? '0');
-        solveCell(cell.rowIndex, cell.columnIndex, solution);
-    };
+  const handleClick = (): void => {
+    setEditMode(true);
+  };
 
-    const getClassName = (value: string): string => {
-        return `${value} row${cell.rowIndex + 1} column${cell.columnIndex + 1}`;
-    };
-
-    // If solved, show solution
-    if (cell.solution) {
-        return (
-            <td className={getClassName('solution')}>{cell.solution}</td>
-        );
+  const handleEditKeyUp = (e: any): void => {
+    if (!isNaN(e.key)) {
+      const solution = parseInt(e.key);
+      solveCell(cell.rowIndex, cell.columnIndex, solution);
     }
 
-    // otherwise, show possibles
+    setEditMode(false);
+  };
+
+  const getClassName = (value: string): string => {
+    return `${value} row${cell.rowIndex + 1} column${cell.columnIndex + 1}`;
+  };
+
+  // If solved, show solution
+  if (cell.solution) {
+    return <td className={getClassName("solution")}>{cell.solution}</td>;
+  }
+
+  // Edit mode?
+  if (editMode) {
     return (
-        <td className={getClassName('pencil')} onClick={handleClick}>
-            {cell.possibleSolutions.map((solution, index) => (
-                <span key={`r${cell.rowIndex}c${cell.columnIndex}s${solution}`}>
-                    <span>{solution}</span>
-                    <span> </span>
-                </span>
-            ))}
-        </td>
+      <td>
+        <input type="text" autoFocus onKeyUpCapture={handleEditKeyUp} />
+      </td>
     );
-}
+  }
+
+  // otherwise, show possibles
+  return (
+    <td className={getClassName("pencil")} onClick={handleClick}>
+      {cell.possibleSolutions.map((solution, index) => (
+        <span key={`r${cell.rowIndex}c${cell.columnIndex}s${solution}`}>
+          <span>{solution}</span>
+          <span> </span>
+        </span>
+      ))}
+    </td>
+  );
+};
 
 export default Cell;
